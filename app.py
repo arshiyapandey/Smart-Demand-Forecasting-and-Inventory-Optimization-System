0c591f3fe0766dac4c60dd345ad3e9ce7a9ce20b
Shruti <swadnerkar18@gmail.com>
2026-04-10 22:08:17 +0530

    add vercel url to CORS and updated title

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://your-app-name.vercel.app"   # replace after Vercel deploy
+        "https://smart-demand-forecasting-and-invent.vercel.app/"   
     ],
     allow_methods=["*"],
     allow_headers=["*"],
